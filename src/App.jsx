1320f27c617e1588bcdf5827c8da4714887d4a51
Paweł Floryan <81591050+pawelfloryan@users.noreply.github.com>
2023-09-30 16:16:38 +0200

    Changed structure and more
    Co-Authored-By: BartoszTurczynski <81752668+BartoszTurczynski@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next';
-import Form from './Form.jsx';
+import Form from './components/Form/Form.jsx';
 import axios from 'axios';
 
 function App() {
@@ -9,13 +9,13 @@ function App() {
   const switchToPolish = () => i18n.changeLanguage('pl');
   const switchToEnglish = () => i18n.changeLanguage('en');
   const host = import.meta.env.VITE_BACKEND;
-  axios.get("http://localhost:3000")
+  /*axios.get("http://localhost:3000")
     .then(response => {
       console.log(response.data);
     })
     .catch(error => {
       console.error('Error fetching data:', error);
-    });
+    });*/
   return (
     <>
       <div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { Dropdown } from "primereact/dropdown";
-import './i18n';
+import { InputSwitch } from "primereact/inputswitch";
+import '../../i18n';
 import axios from 'axios';
 
 function Form() {
+
+    // pomysły na fieldy: stopień studiów ( 1 | 2 ), dla nauczyciela,
+
     const [profile, setProfile] = useState(null);
     const [city, setCity] = useState(null);
+    const [checked, setChecked] = useState(true);
+
     const { t, i18n } = useTranslation();
     const switchToPolish = () => i18n.changeLanguage('pl');
     const switchToEnglish = () => i18n.changeLanguage('en');
@@ -26,8 +32,6 @@ function Form() {
         }
     };
 
-
-
     const profiles = [
         { name: 'Informatyka' },
         { name: 'Prawo' },
@@ -44,6 +48,7 @@ function Form() {
             <form onSubmit={handleSubmit}>
                 <Dropdown value={profile} onChange={(e) => setProfile(e.value)} options={profiles} optionLabel="name" placeholder="Wybierz profil" />
                 <Dropdown value={city} onChange={(e) => setCity(e.value)} options={cities} optionLabel="name" placeholder="Wybierz profil" />
+                <InputSwitch checked={checked} onChange={(e) => setChecked(e.value)} />
             </form>
         </>
     )
