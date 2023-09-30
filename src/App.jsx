afebd7d067ee516f1621b271fa728fcf66a061e2
BartoszTurczynski <bart.turczynski@gmail.com>
2023-09-30 18:07:54 +0200

    english

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import Form from './components/Form/Form.jsx';
-import axios from 'axios';
-import pol from './assets/polish.png';
-import eng from './assets/english.png';
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import Form from "./components/Form/Form.jsx";
+import axios from "axios";
+import pol from "./assets/polish.png";
+import eng from "./assets/english.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const { t, i18n } = useTranslation();
-  const switchToPolish = () => i18n.changeLanguage('pl');
-  const switchToEnglish = () => i18n.changeLanguage('en');
+  const switchToPolish = () => i18n.changeLanguage("pl");
+  const switchToEnglish = () => i18n.changeLanguage("en");
   const host = import.meta.env.VITE_BACKEND;
   /*axios.get("http://localhost:3000")
     .then(response => {
@@ -21,15 +21,17 @@ function App() {
     });*/
   return (
     <>
-      <img className="lang" src={pol} onClick={switchToPolish} />
-      <img className="lang" src={eng} onClick={switchToEnglish} />
+      <div>
+        <img className="lang" src={pol} onClick={switchToPolish} />
+        <img className="lang" src={eng} onClick={switchToEnglish} />
+      </div>
       <div className="text">
-        <h1 className="mainTxt">Co ja ze sobą zrobię?</h1>
-        <h2>{t("Do jakiej uczelni pójdziesz?")}</h2>
+        <h1 className="mainTxt">{t("title")}</h1>
+        <h2>{t("question")}</h2>
       </div>
       <Form />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,73 +1,90 @@
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next';
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Dropdown } from "primereact/dropdown";
 import { InputSwitch } from "primereact/inputswitch";
-import '../../i18n';
-import axios from 'axios';
-import './Form.module.css';
+import "../../i18n";
+import axios from "axios";
+import "./Form.module.css";
 
 function Form() {
+  // pomysły na fieldy: stopień studiów ( 1 | 2 ), dla nauczyciela, język w którym jest prowadzone, czy są dualne
 
-    // pomysły na fieldy: stopień studiów ( 1 | 2 ), dla nauczyciela, język w którym jest prowadzone, czy są dualne 
+  const [profile, setProfile] = useState(null);
+  const [city, setCity] = useState(null);
+  const [canTeacher, setCanTeacher] = useState(false);
+  const [degree, setDegree] = useState(null);
 
-    const [profile, setProfile] = useState(null);
-    const [city, setCity] = useState(null);
-    const [canTeacher, setCanTeacher] = useState(false);
-    const [degree, setDegree] = useState(null);
+  const { t, i18n } = useTranslation();
+  const switchToPolish = () => i18n.changeLanguage("pl");
+  const switchToEnglish = () => i18n.changeLanguage("en");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const { t, i18n } = useTranslation();
-    const switchToPolish = () => i18n.changeLanguage('pl');
-    const switchToEnglish = () => i18n.changeLanguage('en');
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        const userData = {
-            profile: profile,
-            city: city
-        };
-
-        try {
-            const response = await axios.post('https://api.example.com/users', userData);
-            console.log('Data sent successfully:', response.data);
-        } catch (error) {
-            console.error('Error sending data:', error);
-        }
+    const userData = {
+      profile: profile,
+      city: city,
     };
 
-    const profiles = [
-        { name: 'Informatyka' },
-        { name: 'Prawo' },
-        { name: 'Automatyka' }
-    ];
+    try {
+      const response = await axios.post(
+        "https://api.example.com/users",
+        userData
+      );
+      console.log("Data sent successfully:", response.data);
+    } catch (error) {
+      console.error("Error sending data:", error);
+    }
+  };
 
-    const cities = [
-        { name: 'Kraków' },
-        { name: 'Warszawa' }
-    ];
+  const profiles = [{ name: t("IT") }, { name: t("law") }, { name: t("aut") }];
 
-    const degrees = [
-        { name: 1 },
-        { name: 2 }
-    ];
+  const cities = [{ name: t("kr") }, { name: t("wa") }];
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <div className="card flex justify-content-center">
-                        <Dropdown value={profile} onChange={(e) => setProfile(e.value)} options={profiles} className="w-full md:w-14rem" optionLabel="name" placeholder="Wybierz profil" editable />
-                    </div>
-                    <div className="card flex justify-content-center">
-                        <Dropdown value={city} onChange={(e) => setCity(e.value)} options={cities} optionLabel="name" placeholder="Wybierz miasto" editable />
-                    </div>
-                    <div className="card flex justify-content-center">
-                        <Dropdown value={degree} onChange={(e) => setDegree(e.value)} options={degrees} optionLabel="name" placeholder="Którego stopnia" editable />
-                    </div>
-                </div>
-                <InputSwitch checked={canTeacher} onChange={(e) => setCanTeacher(e.value)} />
-            </form>
-        </>
-    )
+  const degrees = [{ name: t("ba") }, { name: t("ma") }];
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div className="card flex justify-content-center">
+            <Dropdown
+              value={profile}
+              onChange={(e) => setProfile(e.value)}
+              options={profiles}
+              className="w-full md:w-14rem"
+              optionLabel="name"
+              placeholder={t("f1")}
+              editable
+            />
+          </div>
+          <div className="card flex justify-content-center">
+            <Dropdown
+              value={city}
+              onChange={(e) => setCity(e.value)}
+              options={cities}
+              optionLabel="name"
+              placeholder={t("f2")}
+              editable
+            />
+          </div>
+          <div className="card flex justify-content-center">
+            <Dropdown
+              value={degree}
+              onChange={(e) => setDegree(e.value)}
+              options={degrees}
+              optionLabel="name"
+              placeholder={t("f3")}
+              editable
+            />
+          </div>
+        </div>
+        <InputSwitch
+          checked={canTeacher}
+          onChange={(e) => setCanTeacher(e.value)}
+        />
+      </form>
+    </>
+  );
 }
 
-export default Form
+export default Form;
